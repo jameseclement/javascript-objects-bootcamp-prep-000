73bd6b2fc190eb0794eddcc17cc8375b7b7111e3
jameseclement <jameseclement@gmail.com>
2018-12-11 20:20:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {artistName: 'songTitle'}
 
 function updatePlaylist (playlist, artistName, songTitle){
@@ -9,4 +10,11 @@ function removeFromPlaylist (playlist, artistName){
   delete playlist[artistName]
 return playlist
   
+=======
+var playlist = {artist: 'song'}
+
+function updatePlaylist (playlist, artistName, songTitle){
+  playlist.artistName = 'songTitle'
+  return playlist
+>>>>>>> 1474c38fc20b043ef7c8a8b22888581866010163
 }
